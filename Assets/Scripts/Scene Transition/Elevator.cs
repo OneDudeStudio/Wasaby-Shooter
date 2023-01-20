1e946da5a04e0ab0fc9fbec651854a3eb79dee2d
Sacura-ss <72346790+Sacura-ss@users.noreply.github.com>
2023-01-20 13:59:37 +0300

    Create basis of portal. Some comments in Elevator.cs

--- Assets/Scripts/Scene Transition/Elevator.cs
+++ Assets/Scripts/Scene Transition/Elevator.cs
@@ -1,6 +1,7 @@
 using System;
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class Elevator : MonoBehaviour
 {
@@ -8,38 +9,47 @@ public class Elevator : MonoBehaviour
     [SerializeField] private Transform _elevatorDoors;
     [SerializeField] private Transform _player;
     [SerializeField] private GameObject _playerCam;
-    
+
+    /*private void Update()
+    {
+        float cycleLenght = 1f;
+        Quaternion targetRotation = Quaternion.LookRotation(new Vector3(_elevatorDoors.position.x, 0, _elevatorDoors.position.z)
+                                                            - new Vector3(_playerCam.transform.position.x, 0, _playerCam.transform.position.z) * 0.5f);
+        print(targetRotation);
+        _playerCam.transform.DORotateQuaternion(targetRotation, cycleLenght);
+    }*/
 
     private void OnTriggerEnter(Collider other)
     {
         print("in elevator");
-        
+
         // disable camera script
         _playerCam.GetComponent<PlayerCam>().enabled = false;
-        
+
         if (_sceneToSwitch != String.Empty)
         {
-            
             // do some animations 
             PlayLookAtAnimations();
             PlayElevatorDoorsAnimation();
-            
+
             SceneTransition.SwitchToScene(_sceneToSwitch);
-            
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
         print("out elevator");
-        
+
         // enable camera script
         _playerCam.GetComponent<PlayerCam>().enabled = true;
     }
 
     private void PlayLookAtAnimations()
     {
-        float cycleLenght = 1;
+        float cycleLenght = 1f;
+        /*Quaternion targetRotation = Quaternion.LookRotation(new Vector3(_elevatorDoors.position.x, 0, _elevatorDoors.position.z) 
+                                                            - new Vector3(_playerCam.transform.position.x, 0, _playerCam.transform.position.z) * 0.2f );
+        if (_playerCam != null) _playerCam.transform.DORotateQuaternion(targetRotation, cycleLenght);*/
         _player.DORotate(new Vector3(0, 180, 0), cycleLenght);
     }
 
