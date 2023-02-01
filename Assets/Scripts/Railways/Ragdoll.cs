e5a51b1858cd4389f1a7969c1a33cf690a5b1f65
Sacura-ss <72346790+Sacura-ss@users.noreply.github.com>
2023-02-01 15:18:43 +0300

    Implemented a train push

---
+++ Assets/Scripts/Railways/Ragdoll.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+// attach to first person
+public class Ragdoll : MonoBehaviour
+{
+    private Rigidbody[] _rigidBodies;
+    private Rigidbody _rigidbody;
+    private Animator _animator;
+
+    void Start()
+    {
+        _rigidBodies = GetComponentsInChildren<Rigidbody>();
+        _rigidbody = GetComponent<Rigidbody>();
+        _animator = GetComponent<Animator>();
+        //DeactivateRagdoll();
+    }
+
+    public void DeactivateRagdoll()
+    {
+        if(_rigidbody != null) _rigidbody.isKinematic = true;
+        try
+        {
+            foreach (var rigidBody in _rigidBodies)
+            {
+                rigidBody.isKinematic = true;
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+        
+        if (_animator != null) _animator.enabled = true;
+    }
+
+    public void ActivateRagdoll()
+    {
+        if(_rigidbody != null) _rigidbody.isKinematic = false;
+        try
+        {
+            foreach (var rigidBody in _rigidBodies)
+            {
+                rigidBody.isKinematic = false;
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+
+        if (_animator != null) _animator.enabled = false;
+    }
+
+    public void ApplyForce(Vector3 force)
+    {
+        _rigidbody.AddForce(force, ForceMode.VelocityChange);
+    }
+}
--- Assets/Scripts/Railways/TrainCollision.cs
+++ Assets/Scripts/Railways/TrainCollision.cs
@@ -5,9 +5,32 @@ using UnityEngine;
 
 public class TrainCollision : MonoBehaviour
 {
+    [SerializeField] float _pushForce = 5;
+    private Ragdoll _otherObjectRagdoll;
     private void OnCollisionEnter(UnityEngine.Collision collision)
     {
+        _otherObjectRagdoll = collision.gameObject.GetComponent<Ragdoll>();
+        
         Debug.Log("Collision!!! " + collision.gameObject.name);
-        collision.gameObject.transform.Translate(new Vector3(0.2f, 0.3f, 1) * 5 * Time.deltaTime);
+        
+        //TakeDamage();
+        //_otherObjectRagdoll.ActivateRagdoll();
+        PushObject(GetComponent<Move>().directionPoint.transform.position);
+        //_otherObjectRagdoll.DeactivateRagdoll();
+        
     }
+
+    private void PushObject(Vector3 direction)
+    {
+        direction.y = 1;
+        _otherObjectRagdoll.ApplyForce(direction * _pushForce);
+    }
+
+    private void TakeDamage(float amount, Vector3 direction)
+    {
+        // TO DO :: currentHealthOfPlayer -= amount
+        // TO DO :: if (currentHealthOfPlayer <= 0.0f) Die();
+        
+    }
+    
 }
