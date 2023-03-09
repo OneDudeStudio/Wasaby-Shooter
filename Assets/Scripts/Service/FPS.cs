2ed65e8241bc5ab99b9cdb1a8c653503dc2bb8b1
Sacura-ss <sacura-ss@yandex.ru>
2023-03-09 16:53:44 +0300

    Add fps counter

---
+++ Assets/Scripts/Service/FPS.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class FPS : MonoBehaviour
+{
+    private static float fps;
+
+    private void OnGUI()
+    {
+        fps = 1.0f / Time.deltaTime;
+        GUILayout.Label("FPS: " + (int)fps);
+    }
+}
